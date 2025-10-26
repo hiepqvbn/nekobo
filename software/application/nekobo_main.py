563f699321fcc805ad85b3d6df99f56dd5d3c4b5
hiep-desktop <kidaliens@gmail.com>
2025-10-26 09:52:52 +0900

    fix: debug_flag

--- software/application/nekobo_main.py
+++ software/application/nekobo_main.py
@@ -28,7 +28,7 @@ def main():
 
     # Create serial comm (will attempt to auto-find port)
     # Enable verbose serial logging when NEKOBO_DEBUG env var is set.
-    debug_flag = bool(os.environ.get('NEKOBO_DEBUG'))
+    debug_flag = bool(os.environ.get('NEKOBO_DEBUG', False))
     if debug_flag:
         print("Debug mode enabled: verbose serial logging")
         time.sleep(2)
